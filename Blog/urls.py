2ee8e236f63f399826744a151fe3dd51ab493d4f
Nahuel Castro <castronahuel14@gmail.com>
2022-06-17 20:03:13 +0200

    crud categories

--- Blog/urls.py
+++ Blog/urls.py
@@ -19,6 +19,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from categories.api.router import router_categories
+
 
 
 schema_view = get_schema_view(
@@ -40,6 +42,7 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/', include('users.api.router')),
+    path('api/', include(router_categories.urls)),
 
 ]
 
--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from categories.models import *
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'published', 'description', 'created_at', 'updated_at')
+    list_filter = ('published',)
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at'
+
+
