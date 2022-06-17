36d893a8901eafb857f0a37eebbf855e17efe702
Nahuel Castro <castronahuel14@gmail.com>
2022-06-17 05:46:24 +0200

    set up api docs with drf yasg and set up rest_framework

--- Blog/settings.py
+++ Blog/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_yasg',
     'users',
+
 ]
 
 MIDDLEWARE = [
--- Blog/urls.py
+++ Blog/urls.py
@@ -15,7 +15,44 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Blog API",
+      default_version='v1',
+      description="Blog API Documentation",
+      terms_of_service="", #https://www.google.com/policies/terms/
+      contact=openapi.Contact(email="contact@nahuel.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   # permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('jsondocs/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
