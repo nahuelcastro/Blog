8e5274382f7918c291604c455ef403d8fddccecb
Nahuel Castro <castronahuel14@gmail.com>
2022-06-18 14:22:59 +0200

    ampl user and category objects in get posts

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from posts.models import Post
+from users.api.serializers import UserSerializer
+from categories.api.serializers import CategorySerializer
 
 class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    category = CategorySerializer(read_only=True)
     class Meta:
         model = Post
         fields = ['id', 'title', 'slug', 'content', 'published', 'category', 'user', 'created_at', 'updated_at']
