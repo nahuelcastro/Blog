390cec3e2b1aa5f249cd32bcc8f05662542fa434
Nahuel Castro <castronahuel14@gmail.com>
2022-06-18 13:49:19 +0200

    create model Post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models import SET_NULL
 
-# Create your models here.
+from users.models import User
+from categories.models import Category
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    slug = models.SlugField(max_length=255, unique=True)
+    miniature = models.ImageField(upload_to='posts/img/', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=SET_NULL, null=True)
+
