2606003e42c2c8e42b8db267977ba1fb27486e73
Nahuel Castro <castronahuel14@gmail.com>
2022-06-18 12:49:02 +0200

    Set Permissions IsAdminOrReadOnly in categories

--- categories/api/views.py
+++ categories/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from categories.models import Category
 from categories.api.serializers import CategorySerializer
+from categories.api.permissions import IsAdminOrReadOnly
 
 class CategoryApiViewSet(ModelViewSet):
+    permission_classes = [IsAdminOrReadOnly]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     
